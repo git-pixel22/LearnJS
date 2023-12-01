49680629e632443b16d84f1ca77dafcc2a585647
Pixel <84857474+git-pixel22@users.noreply.github.com>
2023-12-01 08:36:28 +0000

    variables

---
+++ basics/variables_boi.js
@@ -0,0 +1,15 @@
+const accountId = 132912
+let accountEmail = "pixel22@wargames.com"
+var accountPassword = "password"
+accountCity = "Delhi"
+let accountState
+
+// accountId = 22 // not allowed, sir.
+
+accountEmail = "papapixel@lessgo.com"
+accountPassword = "whoareyou"
+accountCity = "smd"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
